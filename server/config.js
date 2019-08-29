34852dd2c465846f86c8c79ffd38a4e71f59892d
Adrien M <adrien@d.tube>
2019-08-29 13:58:31 +0200

    move config to env var

---
+++ server/config.js
@@ -0,0 +1,15 @@
+var config = {
+    ssl: process.env.ENABLE_SSL || false,
+    domain: process.env.DOMAIN || "http://localhost:3000",
+    hcaptchaSecret: process.env.HCAPTCHA_SECRET || "aaa",
+    aws: {
+        id: process.env.AWS_ID || "123",
+        secret: process.env.AWS_SECRET || "bbb"
+    },
+    fb: {
+        id: process.env.FB_ID || "456",
+        secret: process.env.FB_SECRET || "ccc"
+    }
+}
+
+module.exports = config
