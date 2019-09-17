167cafbfb2e99f35bf2ef97c133b10417e0d4c7f
Adrien M <adrien@d.tube>
2019-09-17 22:54:56 +0200

    hide token sale

--- server/config.js
+++ server/config.js
@@ -16,7 +16,7 @@ var config = {
         secret: process.env.COINBASE_SECRET || 'eee'
     },
     limits: {
-        maxTokensSold: 1000,
+        maxTokensSold: 0,
         smsCount: 2,
         smsPeriod: 1000*60*60,
         emailCount: 5,
