90e768f5bee268657249ad81c7cf658f4283501a
Adrien M <adrien@d.tube>
2020-11-08 21:02:50 +0100

    more countries

--- client/js/index.js
+++ client/js/index.js
@@ -2,7 +2,25 @@
 myEmail = null
 smsAllowedCountries = [
     1, // USA + Canada
-    91, //India
+    91, // India
+    350, // Gibraltar
+    386, // Slovenia
+    356, // Malta
+    972, // Israel
+    234, // Nigeria
+    27, // South Africa
+    352, // Luxembourg
+    966, // Saudi Arabia
+    66, // Thailand
+    90, // Turkey
+    86, // China
+    971, // UAE
+    41, // Switzerland
+    45, // Denmark
+    44, // United Kingdom
+    82, // South Korea
+    357, // Cyprus
+    65, // Singapore
 ]
 
 startSignup.onclick = function() {
